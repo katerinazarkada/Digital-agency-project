f1584e1f54d1f852aafaf7650b3ab54576f5de64
Katerina <katerinazarkadha@gmail.com>
2026-01-29 21:16:46 +0200

    Ενημέρωσα τον κώδικα στο index και style.css

--- js/main.js
+++ js/main.js
@@ -211,3 +211,18 @@
                 }
             });
         });
+        //  Top Button
+const backToTopBtn = document.getElementById("backToTop");
+
+window.onscroll = function() {
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+        backToTopBtn.style.display = "block";
+    } else {
+        backToTopBtn.style.display = "none";
+    }
+};
+
+backToTopBtn.addEventListener("click", () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+});
+
